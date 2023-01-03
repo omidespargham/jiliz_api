98c33b70cf4d3b6248a5dcee3b2ef6770fe6acd0
omidespargham <omidespargham2001@gmail.com>
2023-01-03 16:08:26 +0330

    scroll api handeled !

--- home/urls_v1.py
+++ home/urls_v1.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path("homesearch-view/", views.HomePageSearchView.as_view(), name='SearchViewForHomePAge'),
     path("homedata/", views.HomePageDataView.as_view(), name='HomePageDataView'),
     path("load-data-all/<int:num_post>/", views.HomePageAllCategoriesView.as_view(), name="AllDataViewHomePage"),
-    path("load-data-category/<int:num_post>/<str:category>/", views.HomePageCategoryDataView.as_view(), name="HomePageDataByCategory"),
-
+    path("scrol-data/<int:num_post>/<str:category_id>/", views.HomePageCategoryDataView.as_view(), name="HomePageDataByCategory"),
 ]
 
