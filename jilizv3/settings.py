6af52ce7c5fca460085c11f6f72756ff3b1d47ed
omidespargham <omidespargham2001@gmail.com>
2023-01-15 17:14:51 +0330

    get to gether other_adverts to gether

--- jilizv3/settings.py
+++ jilizv3/settings.py
@@ -37,9 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-
-    "SetupMenudesignConfig",
+    
     # Internal
     'home.apps.HomeConfig',
     'advert.apps.AdvertConfig',
