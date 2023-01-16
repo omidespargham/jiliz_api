cdd38e336625c2dec07de31794b08a67f93f73f8
omidespargham <omidespargham2001@gmail.com>
2023-01-16 16:46:34 +0330

    makemigrations and the import bug handeled !

--- other_adverts/admin.py
+++ other_adverts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Service,InstallRepairAdvert
-admin.site.register(Service)
+from .models import InstallAndRepairExpertiseArea,InstallRepairAdvert,SetupAndMenuExpertiseArea
+admin.site.register(InstallAndRepairExpertiseArea)
 admin.site.register(InstallRepairAdvert)
-
+admin.site.register(SetupAndMenuExpertiseArea)
 
