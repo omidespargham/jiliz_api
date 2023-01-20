bdd80bb3033ec30cba008381af23cbe23b86bede
omidespargham <omidespargham2001@gmail.com>
2023-01-20 10:30:45 +0330

    LoginView maked and ready for test

--- accounts/views.py
+++ accounts/views.py
@@ -1,24 +1,24 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
 from .serializers import UserSerialzier
+from .models import User,RGScode
 from random import randint
 
-# class LoginView(APIView):
-
-#     def post(self, request):
-#         srz_data = UserSerialzier(request.POST)
-#         if srz_data.is_valid():
-#             the_code = randint(1, 9)
-#             RGScode.objects.create(
-#                 phone_number=form.cleaned_data["phone_number"], code=the_code)
-#             print(the_code)
-#             request.session["user_info"] = {
-#                 "phone_number": form.cleaned_data["phone_number"]}
-#             if request.next:
-#                 return redirect(reverse("accounts:user_login_verify") + "?next=" + request.next)
-#             return redirect("accounts:user_login_verify")
-#         return render(request, self.template_name, {"form": form})
+class LoginView(APIView):
+    def post(self, request):
+        srz_data = UserSerialzier(request.POST)
+        if srz_data.is_valid(): # shouldnt check the phone is unique
+            the_code = randint(1, 9)
+            RGScode.objects.create(
+                phone_number=srz_data.validated_data["phone_number"], code=the_code)
+            print(the_code)
+            # if the session didnt work in DRF get the phone again in user verify view !
+            request.session["user_info"] = {
+                "phone_number": srz_data.validated_data["phone_number"]}
+            return Response(data=srz_data.data)
+        return Response(data=srz_data.errors)
 
 
 # Create your views here.
