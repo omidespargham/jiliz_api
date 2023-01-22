df0d9ecc4a5b4b22c18fb3ee5345625df4c41534
omidespargham <omidespargham2001@gmail.com>
2023-01-22 18:26:59 +0330

    phone validate handeled for user !

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,12 +1,25 @@
 # from rest_framework.serializers import ModelSerializer,Serializer
 from rest_framework import serializers
-from .models import User,RGScode
+from .models import User, RGScode
+
 
 class UserSerialzier(serializers.ModelSerializer):
     class Meta:
-        model= User
+        model = User
         fields = ("phone_number",)
-        
+
+    def validate_phone_number(self, value):
+        if value[0:2] != "09":
+            raise serializers.ValidationError([serializers.ValidationError(
+                "شماره تلفن نامعتبر است", code="invalid"), serializers.ValidationError("تلفن باید با 09 شروع شود", code="required")])
+        try:
+            code = RGScode.objects.get(phone_number=value)
+            code.delete()
+        except RGScode.DoesNotExist:
+            pass
+        return value
+    
+
 
 class UserVerifySerializer(serializers.Serializer):
     phone_number = serializers.CharField(required=True)
