199645547bd0a10366948aa1e33a482871a49fdd
omidespargham <omidespargham2001@gmail.com>
2023-01-29 14:00:55 +0330

    home app bug and requirements updated !

--- home/views.py
+++ home/views.py
@@ -37,7 +37,7 @@ class HomePageCategoryDataView(APIView):
         upper = num_post
         lower = upper - visble
         get_category = Category.objects.get(id=category_id)
-        load_adverts = Advert.objects.filter(category=get_category).order_by('-created_obj')[:upper]
+        load_adverts = Advert.objects.filter(categorys=get_category).order_by('-created_obj')[:upper]
         adverts_srz = GetAdvertSerialiser(load_adverts,many=True).data
         return Response(adverts_srz)
 
