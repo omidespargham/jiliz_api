3e61af629412450480c7badca5373464535a5508
omidespargham <omidespargham2001@gmail.com>
2023-02-10 13:58:20 +0330

    id field of some models changed in admin panel

--- advert/admin.py
+++ advert/admin.py
@@ -2,10 +2,21 @@ from django.contrib import admin
 from . import models
 from mptt.admin import MPTTModelAdmin
 
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ("name_city","id")
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ("name","id")
+
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ("name","id")
+
 admin.site.register(models.Category, MPTTModelAdmin)
-admin.site.register(models.City)
-admin.site.register(models.Country)
-admin.site.register(models.Brand)
+admin.site.register(models.City,CityAdmin)
+admin.site.register(models.Country,CountryAdmin)
+admin.site.register(models.Brand,BrandAdmin)
 admin.site.register(models.Advert)
+
 
 
